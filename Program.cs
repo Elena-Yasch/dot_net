91d69b1230bb20cc2ae391e7c084a9f856369c71
Elena <miss.alenka.93@inbox.ru>
2022-09-06 16:33:06 +0300

    Homework 2 - c#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трехзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine($"Рандомное число: {num}");
+
+// int numX = num /10;
+// int num2 = numX % 10;
+
+// Console.WriteLine($"Вторая цифра числа {num} = {num2}.");
+
+
+// Задача 13: Напиите программу, которая выводит третью цифру заданного числа или сообщает,
+//  что третьей цифры нет.
+
+// int initNum = new Random().Next(10, 100000);
+// Console.WriteLine($"Рандомное число: {initNum}");
+// int num = initNum;
+// if (num < 100)
+// {
+//     Console.WriteLine($"Число {num} не содержит три цифры.");
+// }
+// else
+// {
+//     while (num > 1000)
+//     {
+//     num = num /10;
+//     }
+//     Console.WriteLine($"Третья цифра числа {initNum} = {num % 10}.");
+
+// }
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// являетяся ли этот день выходным. 
+
+Console.WriteLine("Введите день недели от 1 до 7:");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 6 || day == 7)
+{
+    Console.WriteLine($" Указанный день - {day} - является выходным.");
+}
+else
+{
+    Console.WriteLine($" Указананный день - {day} - не выходной.");
+}
